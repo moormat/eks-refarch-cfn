7ea8c1d0086c145aca1812cbdd0ba7e184cb69c9
moormat <moormat@amazon.com>
2019-02-21 10:43:36 -0800

    reload daemonset after applying patch

--- proxied/scripts/apply-proxy-configmap.sh
+++ proxied/scripts/apply-proxy-configmap.sh
@@ -34,3 +34,7 @@ kubectl apply -f proxy-environment-variables.yaml
 kubectl patch -n kube-system  -p '{ "spec": {"template": { "spec": { "containers": [ { "name": "aws-node", "envFrom": [ { "configMapRef": {"name": "proxy-environment-variables"} } ] } ] } } } }' daemonset aws-node
 
 kubectl patch -n kube-system  -p '{ "spec": {"template": { "spec": { "containers": [ { "name": "kube-proxy", "envFrom": [ { "configMapRef": {"name": "proxy-environment-variables"} } ] } ] } } } }' daemonset kube-proxy
+
+kubectl set env daemonset/kube-proxy --namespace=kube-system --from=configmap/proxy-environment-variables --containers='*'
+
+kubectl set env daemonset/aws-node --namespace=kube-system --from=configmap/proxy-environment-variables --containers='*'
