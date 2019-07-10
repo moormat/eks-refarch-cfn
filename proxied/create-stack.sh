7d17d502bdc6673e7518d170a6333b161a7816b9
moormat <moormat@amazon.com>
2019-07-10 11:30:51 -0700

    example name

--- proxied/create-stack.sh
+++ proxied/create-stack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name murcia --template-body file://./proxy-parent.yaml --parameters file://./parentparameters.json --capabilities CAPABILITY_IAM --tags='Key=auto-delete,Value=no'
+aws cloudformation create-stack --stack-name proxydemo --template-body file://./proxy-parent.yaml --parameters file://./parentparameters.json --capabilities CAPABILITY_IAM --tags='Key=auto-delete,Value=no'
